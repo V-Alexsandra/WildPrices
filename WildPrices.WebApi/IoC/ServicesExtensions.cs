6274f2698864057df45fd1a37eed20320c3ab8eb
Alexandra <Alexandra@LAPTOP-SS4D3ECJ>
2023-04-05 12:01:15 +0100

    Add WebApi

---
+++ WildPrices.WebApi/IoC/ServicesExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WildPrices.Data.Contexts.Contracts;
+using WildPrices.Data.Contexts.Implementation;
+using WildPrices.Data.Entities;
+using WildPrices.Data.Repositories.Contracts;
+using WildPrices.Data.Repositories.Implementation;
+
+namespace WildPrices.WebApi.IoC
+{
+    public static class ServicesExtensions
+    {
+        public static IServiceCollection ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddIdentity<UserEntity, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequiredLength = 8;
+            }).AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IPriceHistoryRepository, PriceHistoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ WildPrices.WebApi/Program.cs
@@ -0,0 +1,43 @@
+using WildPrices.WebApi.IoC;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("appsettings.Development.json", optional: false, reloadOnChange: true)
+    .Build();
+
+builder.Services.ConfigureSqlContext(configuration);
+
+builder.Services.ConfigureIdentity(configuration);
+
+//builder.Services.ConfigureAutoMapper();
+
+//builder.Services.ConfigureFluentValidation();
+
+builder.Services.ConfigureRepositories();
+
+//builder.Services.ConfigureServices();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
