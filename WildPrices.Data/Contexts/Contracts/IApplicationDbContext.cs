7e376ea262bb4e12141c39d20d57bae881d1fbfa
Alexandra <Alexandra@LAPTOP-SS4D3ECJ>
2023-04-05 11:47:43 +0100

    Add DbContext

---
+++ WildPrices.Data/Contexts/Contracts/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WildPrices.Data.Entities;
+
+namespace WildPrices.Data.Contexts.Contracts
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<UserEntity> Users { get; set; }
+        public DbSet<PriceHistoryEntity> PriceHistories { get; set; }
+        public DbSet<ProductEntity> Products { get; set; }
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ WildPrices.Data/Contexts/Implementation/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using WildPrices.Data.Contexts.Contracts;
+using WildPrices.Data.Entities;
+
+namespace WildPrices.Data.Contexts.Implementation
+{
+    public class ApplicationDbContext : IdentityDbContext, IApplicationDbContext
+    {
+        public DbSet<UserEntity> Users { get; set; } = null!;
+        public DbSet<PriceHistoryEntity> PriceHistories { get; set; } = null!;
+        public DbSet<ProductEntity> Products { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        public async Task<int> SaveChangesAsync() => await base.SaveChangesAsync();
+    }
+}
