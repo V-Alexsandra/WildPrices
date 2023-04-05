ad044bfbebab12045a7b0124c80b795a194c9d1c
Alexandra <Alexandra@LAPTOP-SS4D3ECJ>
2023-04-05 13:49:08 +0100

    Fix DataLayer

--- WildPrices.Data/Repositories/Contracts/IPriceHistoryRepository.cs
+++ WildPrices.Data/Repositories/Contracts/IPriceHistoryRepository.cs
@@ -5,6 +5,7 @@ namespace WildPrices.Data.Repositories.Contracts
     public interface IPriceHistoryRepository : IBaseRepository<PriceHistoryEntity>
     {
         Task DeleteAllByProductIdAsync(int productId);
+
         Task<IEnumerable<PriceHistoryEntity>> GetAllByProductIdAsync(int productId);
 
         Task<double> GetTheCurrentPriceAsync(int productId);
