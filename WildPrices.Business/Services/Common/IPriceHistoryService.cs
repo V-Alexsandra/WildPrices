b5cae1bb7b5a0a92b0ea6bb00a8cc4459abff588
Alexandra <Alexandra@LAPTOP-SS4D3ECJ>
2023-04-06 12:51:56 +0100

    Fix IPriceHistoryRepository

---
+++ WildPrices.Business/Services/Common/IPriceHistoryService.cs
@@ -0,0 +1,18 @@
+﻿using WildPrices.Business.DTOs.PriceHistoryDtos;
+using WildPrices.Business.DTOs.ProductDtos;
+
+namespace WildPrices.Business.Services.Common
+{
+    public interface IPriceHistoryService
+    {
+        Task CreatePriceHistoryAsync(PriceHistoryDto model);
+
+        Task<CurrentPriceDto> GetCurrentPriceForProductAsync(ProductForViewDto model);
+
+        Task<CurrentPriceDto> GetCurrentPriceForProductCardAsync(ProductCardForViewDto model);
+
+        Task<IEnumerable<PriceHistoryDto>> GetPriceHistoryForProductCardAsync(ProductCardForViewDto model);
+
+        Task DeleteAllByProductIdAsync(ProductForViewDto model);
+    }
+}
