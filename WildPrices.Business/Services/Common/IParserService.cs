a4b876e0dd25510709718ab1d7b17cd808e92709
Alexandra <Alexandra@LAPTOP-SS4D3ECJ>
2023-04-07 17:50:23 +0100

    Add ParserService

---
+++ WildPrices.Business/Services/Common/IParserService.cs
@@ -0,0 +1,9 @@
+﻿using WildPrices.Business.DTOs.ProductDtos;
+
+namespace WildPrices.Business.Services.Common
+{
+    public interface IParserService
+    {
+        Task<ProductFromWildberriesDto> GetProductByArticleAsync(string article);
+    }
+}
---
+++ WildPrices.Business/Services/Implementation/ParserService.cs
@@ -0,0 +1,43 @@
+﻿using AngleSharp;
+using WildPrices.Business.DTOs.ProductDtos;
+using WildPrices.Business.Services.Common;
+
+namespace WildPrices.Business.Services.Implementation
+{
+    public class ParserService : IParserService
+    {
+        public async Task<ProductFromWildberriesDto> GetProductByArticleAsync(string article)
+        {
+            string url = $"https://www.wildberries.by/product?card={article}";
+
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+
+            var document = await context.OpenAsync(url);
+
+            string brandName = document.QuerySelector("span.product__brand-name").TextContent.Trim();
+            string productName = document.QuerySelector("span[data-tag=\"productName\"]").TextContent.Trim();
+
+            string name = brandName + " / " + productName;
+
+            string image = document.QuerySelector("picture img.swiper-slide__img").GetAttribute("src");
+            // Создаем объект
+
+            string currentPrice = document.QuerySelector("div.product-price-current span.product-price-current__value").TextContent.Trim();
+
+            currentPrice = currentPrice.Replace(",", ".");
+
+            var productFromWildberries = new ProductFromWildberriesDto
+            {
+                Name = name,
+                Link = url,
+                Image = image,
+                Article = Convert.ToInt32(article),
+                CurrentPrice = Convert.ToDouble(currentPrice),
+                CurrentDate = DateTime.Now.ToString()
+            };
+
+            return productFromWildberries;
+        }
+    }
+}
