8c8a4b268d025f965fa361065984f71195f81e47
Alexandra <Alexandra@LAPTOP-SS4D3ECJ>
2023-04-19 14:30:24 +0100

    Add validators

---
+++ WildPrices.Business/Validators/LoginUserDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using WildPrices.Business.DTOs.UserDtos;
+
+namespace WildPrices.Business.Validators
+{
+    public class LoginUserDtoValidator<T> : AbstractValidator<T> where T : LoginUserDto
+    {
+        public LoginUserDtoValidator()
+        {
+            RuleFor(l => l.Email)
+                .NotNull().WithMessage("Email is required field.");
+
+            When(l => l.Email is not null, () =>
+            {
+                RuleFor(l => l.Email)
+                    .EmailAddress().WithMessage("The entered value is not an email.");
+            });
+
+            RuleFor(l => l.Password)
+                .NotEmpty()
+                .Length(8, 50)
+                .Matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&{}\\[\\]()amp;-])[A-Za-z\\d@$!%*#?&{}\\[\\]()amp;-]{8,}$")
+                .WithMessage("The entered password must have minimum 8 characters at least 1 alphabet, 1 number and 1 special Character.");
+        }
+    }
+}
